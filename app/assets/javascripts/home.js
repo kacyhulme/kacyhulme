f56320da49ed8effaee17860f97ed0afd621d433
kacy hulme <kacyhulme@gmail.com>
2014-06-06 09:01:21 -0500

    more styling changes

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,12 +1,12 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
 
 
-$(document).ready(function(){
-	$("#img").on("mouseenter", function() {
-		$(this).addClass('highlight');
-		}).on("mouseleave", function() {
-		$(this).removeClass('highlight');
-	});
-});
+// $(document).ready(function(){
+// 	$("#img").on("mouseenter", function() {
+// 		$(this).addClass('highlight');
+// 		}).on("mouseleave", function() {
+// 		$(this).removeClass('highlight');
+// 	});
+// });
