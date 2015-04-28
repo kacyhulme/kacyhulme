e5ad497a4a96654af6eb9ea3987342fc8a2b71c4
kacy hulme <kacyhulme@gmail.com>
2015-04-28 10:26:59 -0500

    separated out files to adhere to sass file structure and added and changed styling

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,11 +2,10 @@
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
 
-
-$(document).ready(function(){
-  $("div.thumbnail").on("mouseenter", function() {
-    $(this).addClass('highlight');
-  }).on("mouseleave", function() {
-    $(this).removeClass('highlight');
-  });
-});
+  $(".thumbnail")
+  .on("mouseenter", function() {
+      $(this).addClass('popout');
+    })
+  .on("mouseleave", function() {
+      $(this).removeClass('popout');
+    });
