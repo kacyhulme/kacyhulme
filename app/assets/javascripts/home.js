c6968222d866613fe4a2595c180afdaf69a0a17a
Kacy Hulme (kacyh) <kacyhulme@gmail.com>
2016-11-25 14:15:44 -0600

    edit bouncing cells

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,7 +2,7 @@
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
 
-$(".cells")
+$(".bounceme")
 .on("mouseenter", function() {
   $(this).addClass('popout');
 })
