56724e783bf8cf386be0cf26c740bf94ff6334f3
Kacy Hulme (kacyh) <kacyhulme@gmail.com>
2016-12-10 15:57:42 -0600

    css testing is now working

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -9,13 +9,13 @@ $(window).load(function() {
   });
 });
 
-$(".modal-button").hide();  
+$(".show-overlay-button").hide();  
 
 $(".click-me-button").click(function() {
-  $(".intro-div").toggle();
-  $(".show-directions-button").toggle();
-  $(".description-div").toggle();  
-  $(".modal-button").toggle();  
+  $(".welcome-div").toggle();
+  $(".show-overlay-button").toggle();
+  $(".intro-div").toggle();  
+  $(".show-overlay-button").toggle();
 })
 
 
