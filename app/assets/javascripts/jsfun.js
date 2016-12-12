5f15feffec58bd83b31218f845d275dfb8699fbb
Kacy Hulme (kacyh) <kacyhulme@gmail.com>
2016-12-12 11:46:04 -0600

    add more js to get going

--- app/assets/javascripts/jsfun.js
+++ app/assets/javascripts/jsfun.js
@@ -1,4 +1,12 @@
+var transformName = function(name) {
+  alert(name);
+}
 
+var getUserName = function() {
+  var userName = $('#example-text-input').val()
+  transformName(userName);
+};
 
+var submitButton = $('#name-submit-button');
 
-
+submitButton.click(getUserName);
