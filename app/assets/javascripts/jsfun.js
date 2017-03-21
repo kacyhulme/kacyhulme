3196b61b78b95b7898088628983ae9ec1039ead9
Kacy Hulme (kacyh) <kacyhulme@gmail.com>
2017-03-20 22:03:01 -0500

    add more js

--- app/assets/javascripts/jsfun.js
+++ app/assets/javascripts/jsfun.js
@@ -1,11 +1,13 @@
-var transformName = function(name) {
-  for (var i = 0, len = name.length; i < len; i++) {
-   $('.print-name').append(name[i]);
-  }
+$( document ).ready(function() {
+  var transformName = function(name) {
+    $('.row.print-name').append("Hi! ")
+    for (var i = 0, len = name.length; i < len; i++) {
+     $('.print-name').append(name[i]);
+   }
 
-}
+ }
 
-var getUserName = function() {
+ var getUserName = function() {
   var userName = $('#example-text-input').val()
   transformName(userName);
 };
@@ -13,3 +15,4 @@ var getUserName = function() {
 var submitButton = $('#name-submit-button');
 
 submitButton.click(getUserName);
+});
