33c3a4e52e8a7f8605c74d0fe204015ba29e2132
Hulme <kacy.hulme@gmail.com>
2017-05-30 17:01:16 -0500

    edit to react components and templates

--- app/assets/javascripts/optitest.js
+++ app/assets/javascripts/optitest.js
@@ -1,16 +1,16 @@
-var xhr = new XMLHttpRequest();
+// var xhr = new XMLHttpRequest();
 
-$(document).ready(function() {
-  $.ajax({
-    url: 'https://api.optimizely.com/v2/projects',
-    type: 'GET',
-    dataType: 'json',
-    success: function(data) { console.log(data); },
-    //error: function() { alert('boo!'); },
-    beforeSend: setHeader
-  });
-});
+// $(document).ready(function() {
+//   $.ajax({
+//     url: 'https://api.optimizely.com/v2/projects',
+//     type: 'GET',
+//     dataType: 'json',
+//     success: function(data) { console.log(data); },
+//     //error: function() { alert('boo!'); },
+//     beforeSend: setHeader
+//   });
+// });
 
-function setHeader (xhr) {
-  xhr.setRequestHeader('Authorization', 'Bearer **my auth code here**');
-}
+// function setHeader (xhr) {
+//   xhr.setRequestHeader('Authorization', 'Bearer **my auth code here**');
+// }
