300dae2a0a278c4a6b463bcb9a446dfef5ed9942
kacy hulme <kacyhulme@gmail.com>
2018-03-28 04:31:48 -0500

    add overlay on click

--- app/assets/javascripts/samples/index.js
+++ app/assets/javascripts/samples/index.js
@@ -1,4 +1,20 @@
 $( document ).ready(function() {
+    $('.image-card img').mouseover(function() {
+        $(this).css('transform', 'translate(3px,3px)');
+    });
+    $('.image-card img').mouseout(function() {
+        $(this).css('transform', 'translate(-3px,-3px)');
+    });
+    $('.image-card').click(function() {
+        $('.overlay').css('display','block');
+        var getdataelement = $(this).attr("data-img");
+        $('.inner-overlay').css('background-image','url(portfolio/ao' + getdataelement+'.png)');
+    });
+    $('.overlay').click(function() {
+        $(this).css('display','none');
+    });
 
-console.log("lskdjflsdkf");
+    //$( ".image-card" ).filter(function() {} );
+
+    //$(".image-card").find("[data-img='" + current + "']");
 });
