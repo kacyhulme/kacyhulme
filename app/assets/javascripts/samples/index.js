4ff5527aff62b2e5983f63f582fb28bf8eb05c5d
kacy hulme <kacyhulme@gmail.com>
2018-06-18 10:57:57 -0500

    img updates

--- app/assets/javascripts/samples/index.js
+++ app/assets/javascripts/samples/index.js
@@ -6,12 +6,25 @@ $( document ).ready(function() {
         $(this).css('transform', 'translate(-3px,-3px)');
     });
     $('.image-card').click(function() {
+        $('.overlay').scrollTop(0);
         $('.overlay').css('display','block');
+        $('.overlay').css('overflow-y', 'scroll');
+        $('.inner-overlay').css('display','block');
+        $('body').css('overflow','hidden');
         var getdataelement = $(this).attr("data-img");
         $('.inner-overlay').css('background-image','url("assets/portfolio/ao' + getdataelement+'.png")');
-    });
+        console.log(getdataelement);
+        if(getdataelement == "6") {
+            $('.inner-overlay').addClass('data-el-6');
+        }
+        if(getdataelement == "7") {
+            $('.inner-overlay').addClass('data-el-7');        }
+
+        });
     $('.overlay').click(function() {
-        $(this).css('display','none');
+        $('.overlay').css('display', 'none');
+        $('.inner-overlay').css('display', 'none');
+        $('body').css('overflow','visible');
     });
 
     //$( ".image-card" ).filter(function() {} );
