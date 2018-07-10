13b963e259411609d18f5e3d00ab54ea47b09fa7
Kacy Hulme <kacy.hulme@solarwinds.com>
2018-07-10 09:09:03 -0500

    fix scroll buttons

--- app/assets/javascripts/samples/index.js
+++ app/assets/javascripts/samples/index.js
@@ -8,10 +8,11 @@ $(document).ready(function() {
         autoplay: true,
         autoplaySpeed: 3500,
         pauseOnHover: true,
-        mobileFirst: true,
+        //mobileFirst: true,
         appendArrows: $('.nav-wrapper'),
         prevArrow: '<button type="button" class="nav-arrow slick-prev">&#8678;</button>',
         nextArrow: '<button type="button" class="nav-arrow slick-next">&#8680;</button>',
     });
     $('.slick-next').addClass('pull-right');
+    $('.slick-prev').addClass('pull-left');
 });
