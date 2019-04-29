e988dbadd904ea72615a455e1618ce416b2b81e9
kacy hulme <kacyhulme@gmail.com>
2019-04-29 11:05:48 -0500

    add d3

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,4 +16,5 @@
 //= require react_ujs
 //= require components
 //= require samples
+//= require d3
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   
   get "samples/cssfun" => "samples#cssfun"
   get "samples/jsfun" => "samples#jsfun"
+  get "samples/dthree" => "samples#dthree"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
